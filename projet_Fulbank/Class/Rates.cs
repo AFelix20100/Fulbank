d954f55de44c9faeeb84247880c782d5190925fc
StanciuNA <94056154+StanciuNA@users.noreply.github.com>
2022-10-04 15:02:24 +0200

    Uga buga bich bach bo

---
+++ projet_Fulbank/Class/Rates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_Fulbank.Class
+{
+    internal class Rates
+    {
+        public string EUR;
+        public string getUSD()
+        {
+            return this.EUR;
+        }
+    }
+}
+
---
+++ projet_Fulbank/Class/Root.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_Fulbank.Class
+{
+    class Root
+    //Ceci est l'objet envoyé par la requette HTTPS qui contient la réponse JSON de l'API de cryptomonnaies
+    {
+        public List<Data> data {get; set;}
+    }
+}
