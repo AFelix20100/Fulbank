c72f1d4f6f32d1c3069081ea3b66ac74831ba3d5
AFelix20100 <arthur.felix160@gmail.com>
2022-10-06 12:10:37 +0200

    base

---
+++ projet_Fulbank/AchatCrypto2.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace projet_Fulbank
+{
+    partial class Achat_de_cryptomonnaies2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Achat_de_cryptomonnaies2));
+            this.debit = new System.Windows.Forms.Label();
+            this.solde_compte = new System.Windows.Forms.Label();
+            this.titre = new System.Windows.Forms.Label();
+            this.retour = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.soldecompte = new System.Windows.Forms.TextBox();
+            this.montant_debiter = new System.Windows.Forms.TextBox();
+            this.solde = new System.Windows.Forms.Label();
+            this.solderetrait = new System.Windows.Forms.TextBox();
+            this.solde_crypto = new System.Windows.Forms.Label();
+            this.soldecrypto = new System.Windows.Forms.TextBox();
+            this.simulation = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // debit
+            // 
+            this.debit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.debit.Location = new System.Drawing.Point(513, 191);
+            this.debit.Name = "debit";
+            this.debit.Size = new System.Drawing.Size(185, 23);
+            this.debit.TabIndex = 47;
+            this.debit.Text = "Montant à débiter :";
+            // 
+            // solde_compte
+            // 
+            this.solde_compte.AutoSize = true;
+            this.solde_compte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde_compte.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.solde_compte.Location = new System.Drawing.Point(92, 191);
+            this.solde_compte.Name = "solde_compte";
+            this.solde_compte.Size = new System.Drawing.Size(181, 24);
+            this.solde_compte.TabIndex = 46;
+            this.solde_compte.Text = "Solde du compte :";
+            // 
+            // titre
+            // 
+            this.titre.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.titre.Location = new System.Drawing.Point(145, 139);
+            this.titre.Name = "titre";
+            this.titre.Size = new System.Drawing.Size(501, 52);
+            this.titre.TabIndex = 45;
+            this.titre.Text = "Achat de cryptomonnaies";
+            // 
+            // retour
+            // 
+            this.retour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.retour.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.retour.FlatAppearance.BorderSize = 0;
+            this.retour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.retour.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retour.ForeColor = System.Drawing.Color.White;
+            this.retour.Location = new System.Drawing.Point(12, 102);
+            this.retour.Name = "retour";
+            this.retour.Size = new System.Drawing.Size(98, 23);
+            this.retour.TabIndex = 44;
+            this.retour.Text = "Retour";
+            this.retour.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 44);
+            this.pictureBox1.TabIndex = 43;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(12, 24);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(88, 15);
+            this.label14.TabIndex = 42;
+            this.label14.Text = "N° de compte :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(12, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(40, 15);
+            this.label13.TabIndex = 41;
+            this.label13.Text = "Nom :";
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImage = global::projet_Fulbank.Properties.Resources.Logo_avec_bitcoin;
+            this.logo.Location = new System.Drawing.Point(137, -7);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(521, 143);
+            this.logo.TabIndex = 40;
+            this.logo.TabStop = false;
+            // 
+            // soldecompte
+            // 
+            this.soldecompte.Location = new System.Drawing.Point(106, 218);
+            this.soldecompte.Name = "soldecompte";
+            this.soldecompte.Size = new System.Drawing.Size(143, 20);
+            this.soldecompte.TabIndex = 51;
+            // 
+            // montant_debiter
+            // 
+            this.montant_debiter.Location = new System.Drawing.Point(530, 218);
+            this.montant_debiter.Name = "montant_debiter";
+            this.montant_debiter.Size = new System.Drawing.Size(143, 20);
+            this.montant_debiter.TabIndex = 52;
+            // 
+            // solde
+            // 
+            this.solde.AutoSize = true;
+            this.solde.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.solde.Location = new System.Drawing.Point(81, 298);
+            this.solde.Name = "solde";
+            this.solde.Size = new System.Drawing.Size(192, 24);
+            this.solde.TabIndex = 54;
+            this.solde.Text = "Solde après retrait :";
+            // 
+            // solderetrait
+            // 
+            this.solderetrait.Location = new System.Drawing.Point(106, 325);
+            this.solderetrait.Name = "solderetrait";
+            this.solderetrait.Size = new System.Drawing.Size(143, 20);
+            this.solderetrait.TabIndex = 53;
+            // 
+            // solde_crypto
+            // 
+            this.solde_crypto.AutoSize = true;
+            this.solde_crypto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde_crypto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.solde_crypto.Location = new System.Drawing.Point(489, 298);
+            this.solde_crypto.Name = "solde_crypto";
+            this.solde_crypto.Size = new System.Drawing.Size(230, 24);
+            this.solde_crypto.TabIndex = 56;
+            this.solde_crypto.Text = "Solde cryptomonnaies :";
+            // 
+            // soldecrypto
+            // 
+            this.soldecrypto.Location = new System.Drawing.Point(530, 325);
+            this.soldecrypto.Name = "soldecrypto";
+            this.soldecrypto.Size = new System.Drawing.Size(143, 20);
+            this.soldecrypto.TabIndex = 55;
+            // 
+            // simulation
+            // 
+            this.simulation.AutoSize = true;
+            this.simulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simulation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.simulation.Location = new System.Drawing.Point(260, 428);
+            this.simulation.Name = "simulation";
+            this.simulation.Size = new System.Drawing.Size(300, 25);
+            this.simulation.TabIndex = 57;
+            this.simulation.Text = "Ceci n\'est pas une simulation ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(622, 377);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 61);
+            this.pictureBox2.TabIndex = 58;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(651, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 59;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Achat_de_cryptomonnaies2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.simulation);
+            this.Controls.Add(this.solde_crypto);
+            this.Controls.Add(this.soldecrypto);
+            this.Controls.Add(this.solde);
+            this.Controls.Add(this.solderetrait);
+            this.Controls.Add(this.montant_debiter);
+            this.Controls.Add(this.soldecompte);
+            this.Controls.Add(this.debit);
+            this.Controls.Add(this.solde_compte);
+            this.Controls.Add(this.titre);
+            this.Controls.Add(this.retour);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.logo);
+            this.Name = "Achat_de_cryptomonnaies2";
+            this.Text = "Achat_de_cryptomonnaies2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label debit;
+        private System.Windows.Forms.Label solde_compte;
+        private System.Windows.Forms.Label titre;
+        private System.Windows.Forms.Button retour;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.TextBox soldecompte;
+        private System.Windows.Forms.TextBox montant_debiter;
+        private System.Windows.Forms.Label solde;
+        private System.Windows.Forms.TextBox solderetrait;
+        private System.Windows.Forms.Label solde_crypto;
+        private System.Windows.Forms.TextBox soldecrypto;
+        private System.Windows.Forms.Label simulation;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ projet_Fulbank/Class/AppelHTTPS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Globalization;
+using projet_Fulbank.Class;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+
+namespace projet_Fulbank
+{
+
+     class AppelHTTPS
+    {
+        public string ApiKey { get; set; }
+
+
+        static public RootApiEuro getEuro()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Accepts", "application / json");
+            string reponse = client.DownloadString("https://api.exchangerate.host/latest?base=USD");
+            RootApiEuro root = JsonConvert.DeserializeObject<RootApiEuro>(reponse);
+            return root;
+        }
+        static public Root RequeteHTTPS() {
+        
+        WebClient client = new WebClient();
+        client.Headers.Add("Accepts", "application/json");
+        string reponse = client.DownloadString("https://api.coincap.io/v2/assets?limit=3");
+        Root RepApp = JsonConvert.DeserializeObject<Root>(reponse);
+        float rootApiEuro = float.Parse(getEuro().rates.getEUR().Replace('.', ','));
+        for (int indexCryp = 0; indexCryp < RepApp.data.Count; indexCryp++)    
+            {   
+            RepApp.data[indexCryp].priceUsd = (float.Parse(RepApp.data[indexCryp].priceUsd.Replace('.',',')) * rootApiEuro).ToString(); 
+            }
+            return RepApp;
+        }
+
+    }
+}
+ 
