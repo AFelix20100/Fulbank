d828ce414cc8440ab980fe213dac8663ba67f6fa
CamillePerier <cperier28@gmail.com>
2023-02-28 15:15:33 +0100

    modif

--- projet_Fulbank/Class/Model/OperationManager.cs
+++ projet_Fulbank/Class/Model/OperationManager.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using projet_Fulbank.Class;
 using System.Drawing.Printing;
+using Org.BouncyCastle.Crypto.Tls;
 
 namespace projet_Fulbank.Class.Model
 {
@@ -66,14 +67,14 @@ namespace projet_Fulbank.Class.Model
             command.Parameters.Add(param4);
             command.Parameters.Add(param5);
             command.Parameters.Add(param6);
-           
+
 
 
             reader = command.ExecuteReader();
             pdo.Close();
         }
 
-        public static void TransferToBeneficiary ( double anAmount, string anIban) //Transfert vers un compte extérieur
+        public static void TransferToBeneficiary(double anAmount, string anIban) //Transfert vers un compte extérieur
         {
             pdo.Open();
             command = pdo.CreateCommand();
@@ -84,16 +85,16 @@ namespace projet_Fulbank.Class.Model
             param1.Value = anAmount;
             MySqlParameter param2 = new MySqlParameter();
             param2.ParameterName = "@anIban";
-            param2.DbType = DbType.String; 
+            param2.DbType = DbType.String;
             param2.Value = anIban;
             command.Parameters.Add(param1);
             command.Parameters.Add(param2);
             reader = command.ExecuteReader();
             pdo.Close();
 
-        } 
+        }
 
-        public static void TransferCurrentToSaving ( double anAmount ) // Transfert du compte courant vers le compte épargne
+        public static void TransferCurrentToSaving(double anAmount) // Transfert du compte courant vers le compte épargne
         {
             pdo.Open();
             command = pdo.CreateCommand();
@@ -109,17 +110,17 @@ namespace projet_Fulbank.Class.Model
             command.Parameters.Add(param);
             command.Parameters.Add(param1);
             reader = command.ExecuteReader();
-            pdo.Close( );  
+            pdo.Close();
 
             pdo.Open();
             command = pdo.CreateCommand();
             command.CommandText = "UPDATE set sold = sold + @anAmount WHERE idTypeOfAccount = 2 AND idPerson = (SELECT id FROM Person WHERE login = @login)";
             reader = command.ExecuteReader();
-            pdo.Close ();
+            pdo.Close();
 
         }
 
-        public static void TransferSavingToCurrent ( double anAmount ) // Transfert du coompte épargne au compte courant
+        public static void TransferSavingToCurrent(double anAmount) // Transfert du coompte épargne au compte courant
         {
             pdo.Open();
             command = pdo.CreateCommand();
@@ -135,7 +136,7 @@ namespace projet_Fulbank.Class.Model
             command.Parameters.Add(param);
             command.Parameters.Add(param1);
             reader = command.ExecuteReader();
-            pdo.Close();    
+            pdo.Close();
 
             pdo.Open();
             command = pdo.CreateCommand();
@@ -144,7 +145,11 @@ namespace projet_Fulbank.Class.Model
             pdo.Close();
         }
 
-
-
+        public static void OperationTransfertBeneficiary(double anAmount)
+        {
+            pdo.Open();
+            command = pdo.CreateCommand();
+            command.CommandText = " INERT ";
+        }
     }
 }
