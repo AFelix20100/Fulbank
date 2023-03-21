ec1f3c1103b9462650c8fd1c12608eaad07c2b32
CamillePerier <cperier28@gmail.com>
2023-03-21 15:54:50 +0100

    changement

--- projet_Fulbank/Releve.Designer.cs
+++ projet_Fulbank/Releve.Designer.cs
@@ -29,30 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Releve));
-            this.titre = new System.Windows.Forms.Label();
             this.retour = new System.Windows.Forms.Button();
             this.label14 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.logo = new System.Windows.Forms.PictureBox();
-            this.amount_current = new System.Windows.Forms.Label();
-            this.sold_current = new System.Windows.Forms.TextBox();
-            this.amount_savings = new System.Windows.Forms.Label();
-            this.sold_savings = new System.Windows.Forms.TextBox();
+            this.titre = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
-            // 
-            // titre
-            // 
-            this.titre.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
-            this.titre.Location = new System.Drawing.Point(217, 139);
-            this.titre.Name = "titre";
-            this.titre.Size = new System.Drawing.Size(362, 52);
-            this.titre.TabIndex = 64;
-            this.titre.Text = "Relevé de compte";
-            this.titre.Click += new System.EventHandler(this.titre_Click);
             // 
             // retour
             // 
@@ -109,55 +94,22 @@
             this.logo.TabIndex = 59;
             this.logo.TabStop = false;
             // 
-            // amount_current
+            // titre
             // 
-            this.amount_current.AutoSize = true;
-            this.amount_current.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
-            this.amount_current.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
-            this.amount_current.Location = new System.Drawing.Point(40, 203);
-            this.amount_current.Name = "amount_current";
-            this.amount_current.Size = new System.Drawing.Size(291, 26);
-            this.amount_current.TabIndex = 65;
-            this.amount_current.Text = "Solde du compte courant :";
-            // 
-            // sold_current
-            // 
-            this.sold_current.Enabled = false;
-            this.sold_current.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.sold_current.Location = new System.Drawing.Point(45, 241);
-            this.sold_current.Name = "sold_current";
-            this.sold_current.Size = new System.Drawing.Size(241, 23);
-            this.sold_current.TabIndex = 66;
-            // 
-            // amount_savings
-            // 
-            this.amount_savings.AutoSize = true;
-            this.amount_savings.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
-            this.amount_savings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
-            this.amount_savings.Location = new System.Drawing.Point(456, 203);
-            this.amount_savings.Name = "amount_savings";
-            this.amount_savings.Size = new System.Drawing.Size(298, 26);
-            this.amount_savings.TabIndex = 67;
-            this.amount_savings.Text = "Solde du compte épargne :";
-            // 
-            // sold_savings
-            // 
-            this.sold_savings.Enabled = false;
-            this.sold_savings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.sold_savings.Location = new System.Drawing.Point(461, 244);
-            this.sold_savings.Name = "sold_savings";
-            this.sold_savings.Size = new System.Drawing.Size(241, 23);
-            this.sold_savings.TabIndex = 68;
+            this.titre.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(153)))));
+            this.titre.Location = new System.Drawing.Point(217, 139);
+            this.titre.Name = "titre";
+            this.titre.Size = new System.Drawing.Size(362, 52);
+            this.titre.TabIndex = 64;
+            this.titre.Text = "Relevé de compte";
+            this.titre.Click += new System.EventHandler(this.titre_Click);
             // 
             // Releve
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.sold_savings);
-            this.Controls.Add(this.amount_savings);
-            this.Controls.Add(this.sold_current);
-            this.Controls.Add(this.amount_current);
             this.Controls.Add(this.titre);
             this.Controls.Add(this.retour);
             this.Controls.Add(this.pictureBox1);
@@ -176,15 +128,11 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label titre;
         private System.Windows.Forms.Button retour;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.PictureBox logo;
-        private System.Windows.Forms.Label amount_current;
-        private System.Windows.Forms.TextBox sold_current;
-        private System.Windows.Forms.Label amount_savings;
-        private System.Windows.Forms.TextBox sold_savings;
+        private System.Windows.Forms.Label titre;
     }
 }
